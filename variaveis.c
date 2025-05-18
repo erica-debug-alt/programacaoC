5ea2a48f70d8c4c3f52f14b5ec08d18984c802f2
erica-debug-alt <erica.gomesa9@gmail.com>
2025-05-18 12:59:53 -0300

    Create variaveis.c

---
+++ variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade;
+    int quantidade = 1;
+    float altura = 1.75;
+    double peso = 90.3;
+    char letra = 'A';
+    char nome[20] = 'sergio';
+
+    idade = 25;
+}
